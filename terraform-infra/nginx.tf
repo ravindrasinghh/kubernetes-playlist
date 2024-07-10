81168908453937a7faeaaf8d36cdc046db6856e7
Ravindra Singh <ravindrasinghh.me@gmail.com>
2024-07-10 23:36:19 +0530

    Update and rename argo-cd.tf to nginx.tf

---
+++ terraform-infra/nginx.tf
@@ -0,0 +1,9 @@
+resource "helm_release" "ingress-nginx" {
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-nginx"
+  create_namespace = true
+  version          = "4.10.0"
+  values           = [file("../values-override/nginx.yaml")]
+}
